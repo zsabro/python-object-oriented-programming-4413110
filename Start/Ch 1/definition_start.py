e97ca9adc6588e647c625b369553ff55fd6a4e66
zsabro <67281041+zsabro@users.noreply.github.com>
2025-02-12 23:17:56 +0500

    commit_dtd_12_feb_25
    Completed two sessions learning
    
    1. Create class, attributes and methods
    2. Defining own methods and calling them in the print() function

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,19 @@
 
 
 # TODO: create a basic class
-
+class Book():
+    def __init__ (self, title):
+        self.title = title
+    print("This is the book class")
+    print("Being tested that what prints on the next lines after calling the initializer")
 
 # TODO: create instances of the class
-
+book1 = Book("War and Peace")
+book2 = Book("Cindrella")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
+
+print(book2)
+print(book2.title)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,33 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price, discount):
         self.title = title
         # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.discount = discount
 
     # TODO: create instance methods
-
+    def get_price(self):
+        return self.price
+    
+    def discount_price(self, discount):
+        print(f"""The price before discount was Rs.{self.price}""")
+        self.price = self.price - (self.price * discount)
+        print(f"""The price after discount is Rs.{self.price}""")
+        return self.price
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace","Leo Tolstoy", 1200, 12000, 0.25)
+b2 = Book("The Catcher in the Rye", "Un Known", 136, 560, 0.40)
 
 # TODO: print the price of book1
-
+# print(f"""Book :{b1.title}, Author :{b1.author}, Actual Price Rs.{b1.price} and Pages: {b1.pages}""")
+# print(f""" The price is obtained through the 'Python_method' is : {b1.get_price()}""")
 
 # TODO: try setting the discount
-
+print(b1.discount_price(0.25))
 
 # TODO: properties with double underscores are hidden by the interpreter
